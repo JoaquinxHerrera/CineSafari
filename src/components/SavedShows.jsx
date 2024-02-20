a54c990ec4ae9971b69334f7033a868e27598a21
JoaquinxHerrera <joaquin.xherrera1@gmail.com>
2024-02-20 11:05:03 -0300

    fix profile

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -48,19 +48,23 @@ const SavedShows = () => {
             onClick={slideLeft}
             />
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
-                {movies.map((movie, id) =>(
-                    <div  key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                      <img className='w-full h-auto block max-h-[150px] rounded'src={movie.img ? `https://image.tmdb.org/t/p/w500/${movie.img}` : defaultMovieImage} alt={movie?.title} />
-                      <div className='absolute top-2 left-2  w-[144px] h-[81px] sm:h-[103px] sm:w-[184px] md:w-[224px] md:h-[126px] lg:w-[264px] lg:h-[148px] rounded hover:bg-black/80 opacity-0 hover:opacity-100 text-white border hover:border-white px-4 py-2 hover:border-spacing-2'>
-                        <Link 
-                          to={`/movieDetail/${movie.id}`}
-                          className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                          {movie?.title}
-                        </Link>
-                        <p onClick={()=>deleteShow(movie.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose/></p>
-                      </div>
-                  </div>
-                ))}
+                { movies ? (
+                  movies.map((movie, id) =>(
+                      <div  key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                        <img className='w-full h-auto block max-h-[150px] rounded'src={movie.img ? `https://image.tmdb.org/t/p/w500/${movie.img}` : defaultMovieImage} alt={movie?.title} />
+                        <div className='absolute top-2 left-2  w-[144px] h-[81px] sm:h-[103px] sm:w-[184px] md:w-[224px] md:h-[126px] lg:w-[264px] lg:h-[148px] rounded hover:bg-black/80 opacity-0 hover:opacity-100 text-white border hover:border-white px-4 py-2 hover:border-spacing-2'>
+                          <Link 
+                            to={`/movieDetail/${movie.id}`}
+                            className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                            {movie?.title}
+                          </Link>
+                          <p onClick={()=>deleteShow(movie.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose/></p>
+                        </div>
+                    </div>
+                ))
+                ) : (
+                  <p>Loading...</p>
+                )}
             </div>
             <MdChevronRight 
             className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' 
